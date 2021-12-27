ed90b23207ffdec4fb47e3ff0cad4eccfaa23b8a
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2021-12-27 13:22:28 +0000

    functions in python

---
+++ 1. Python Refresher/2. Functions and Generators/1. Functions/functions.py
@@ -0,0 +1,8 @@
+# Example function 1: return the sum of two numbers.
+def sum(a, b):
+    return a+b
+
+# Example function 2: return the size of list, and modify the list to now be sorted.
+def list_sort(my_list):
+    my_list.sort()
+    return len(my_list),  my_list
