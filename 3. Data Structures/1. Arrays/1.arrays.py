d9743f82656de8f82a452a29f80086f68ff2d27d
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-03 14:44:01 +0000

    creating an array and printing array using index

---
+++ 3. Data Structures/1. Arrays/1.arrays.py
@@ -0,0 +1,16 @@
+#In Python, we can create a list using square brackets [ ].
+arr_num = [1,2,3]
+print("Printing the array arr-arr_num = ",arr_num)
+arr_char = [1,2,3]
+print("Printing the array arr-arr_char = ",arr_char)
+
+#we can access an item in the list by providing an index for that item
+print(arr_char[0])
+print(arr_char[1])
+print(arr_char[2])
+
+print(arr_num[0])
+print(arr_num[1])
+print(arr_num[2])
+
+#We will ignore general high level functionality like pop() append() insert() to act like normal array as if in other programming languages
