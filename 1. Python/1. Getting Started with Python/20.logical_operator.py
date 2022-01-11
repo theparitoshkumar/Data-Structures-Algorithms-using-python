da2c4ad4d21d804a854da4849f83b9b97f84ef1e
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-11 16:21:07 +0000

    Logical Operator

---
+++ 1. Python/1. Getting Started with Python/20.logical_operator.py
@@ -0,0 +1,21 @@
+#********************Logical Operators*********************
+num1 = 10
+num2 = -20
+num3 = 0
+
+#*****************Logical AND*****************************
+print("True and True:",True and True)
+print("bool(num1 and num2):",bool(num1 and num2))
+print("True and Flase:",True and False)
+print("num1 and num3:",num1 and num3)
+print("False and False:",False and False)
+
+#******************Logical OR*************************
+print("True or True:",True or True)
+print("True or Flase:",True or False)
+print("bool(num1 or num3):",bool(num1 or num3))
+print("False or False:",False or False)
+
+#*************************Logical NOT********************
+print("bool(num1):",bool(num1))
+print("bool(not num1):",bool(not num1))
