2e17e892a9a0289a99fa920085d35baae25e9902
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-11 14:24:42 +0000

    Programs to print variables and add two numbers

---
+++ 1. Python/1. Getting Started with Python/5. print_variables.py
@@ -0,0 +1,7 @@
+""" Write a program to display values of variables in Python. """
+message = "Keep Smiling!"
+print(message)
+userNo = 101
+print("User No is ", userNo)
+gender = 'M'
+print("Gender: ",gender)
---
+++ 1. Python/1. Getting Started with Python/6.add_two_numbers.py
@@ -0,0 +1,6 @@
+""" Write a Python program to find the sum of two numbers """
+
+num1 = 120
+num2 = 40
+result = num1 + num2
+print(result)
