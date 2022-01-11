0aa5d19626c54a73337cc62d3d817ce602bc5328
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-11 14:30:10 +0000

    Memory address of object

---
+++ 1. Python/1. Getting Started with Python/7.address_of_object.py
@@ -0,0 +1,8 @@
+""" Print the memory address of the object """
+num1 = 20
+address1 = id(num1)
+print("Address of num1 is",address1)
+
+num2 = 30 - 10
+address2 = id(num2)
+print("Address of num2 is", address2)
