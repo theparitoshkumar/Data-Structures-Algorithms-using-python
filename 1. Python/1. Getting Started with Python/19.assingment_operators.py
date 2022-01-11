4b4ab1f7b4b837042166d2e2ed81d9dd9cb4339d
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-11 16:11:36 +0000

    assignment operators

---
+++ 1. Python/1. Getting Started with Python/19.assingment_operators.py
@@ -0,0 +1,54 @@
+#***************************Assignment Operators***********************
+
+# = : Assigns value from right-side operand to leftside operand
+num1 = 20
+print("num1 = 20:",num1)
+
+# += :It adds the value of right-side operand to the left-side operand and assigns the result to the left-side operand
+#Note: x += y is same as x = x + y
+num1 += 10
+print("num1 += 10:",num1)
+
+# -= : It subtracts the value of right-side operand from the left-side operand and assigns the result to left-side operand
+#Note: x -= y is same as x = x - y
+num1 -= 5
+print("num1 -= 5:",num1)
+
+# *= : It multiplies the value of right-side operand with the value of left-side operand and assigns the result to left-side operand
+#Note: x *= y is same as x = x * y
+num1 *= 2
+print("num1 *= 2:",num1)
+
+# /= :It divides the value of left-side operand by thevalue of right-side operand and assigns the result to left-side operand
+#Note: x /= y is same as x = x / y
+num1 /= 5
+print("num1 /= 5:",num1)
+
+# %= : It performs modulus operation using two operands and assigns the result to left-side operand
+#Note: x %= y is same as x = x % y
+num1 %= 3
+print("num1 %= 3:",num1)
+
+# //= : It performs floor division using two operands and assigns the result to left-side operand
+#Note: x //= y is same as x = x // y
+num1 //= 2
+print("num1 //= 2:",num1)
+
+# **= : It performs exponential (power) calculation on operators and assigns value to the left-side operand
+#Note: x **= y is same as x = x ** y
+num1 = 2
+num1 **= 10
+print("num1 **= 10:",num1)
+
+
+
+
+
+
+
+
+
+
+
+
+
