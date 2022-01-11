34caee85c61b4005e9cbbd37e58bcef6ba64297f
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-11 15:42:53 +0000

    Arithmetic Operations

---
+++ 1. Python/1. Getting Started with Python/17.arithmetic_operations.py
@@ -0,0 +1,41 @@
+#*****************Arithmetic Operations*********
+
+#*******************Addition********************
+
+#Addition of Numbers
+num1 = 4
+num2 = 5
+print("Addition of Numbers = ",num1+num2)
+
+#Addition of String 
+str1 = "Hello "
+str2 = "World"
+print("Addition of String =",str1+str2)
+
+#**************************Substraction*********************
+print("Substraction of Numbers =",num2-num1)
+
+#**********************Multiplication*****************
+#Multiplication of Numbers
+print("Multiplication of Numbers =",num1*num2)
+
+#Multiplication of String
+print("Multiplication of String =",str1*2)
+
+#***********************Division*****************************
+print("Division of Numbers:", num1/num2)
+
+#**************Modulus***********
+print("Modulus of Numbers:", num1%num2)
+
+#********Floor Division**************
+print("Floor Division of Numbers:",num1//num2 )
+
+#************Exponent***********************
+print("Exponent of Numbers:",num1**num2)
+
+
+
+
+
+
