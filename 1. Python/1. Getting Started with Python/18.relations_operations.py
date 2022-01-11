f00b2892af81336c17ba24ff844ce34788c07f3b
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-11 16:01:09 +0000

    Relational Operation in python

---
+++ 1. Python/1. Getting Started with Python/18.relations_operations.py
@@ -0,0 +1,62 @@
+#******************Relational Operators*********************
+num1 = 10
+num2 = 0
+num3 = 10
+str1 = "Good"
+str2 = "Morning"
+
+#************************Equals to***********************
+#Equals to in numbers
+print("Equals to in numbers:",num1==num2)
+
+#Equals to in String 
+print("Equals to in String:",str1==str2)
+
+#***********************Not equal to**********************
+
+#Not equal to in numbers
+print("Not equal to in numbers:", num1 != num2)
+
+#Not equal to in Strings
+print("Not equal to in Strings:", str1 != str2)
+
+#**************************Greater than**********************
+
+#Greater than in numbers
+print("Greater than in numbers:", num1 > num2)
+
+#Greater than in Strings
+print("Greater than in Strings:", str1 > str2)
+
+#****************Greater than or equal to*************************
+
+#Greater than or equal to in numbers
+print("Greater than or equal to in numbers:",num1 >= num2)
+
+#Greater than or equal to in Strings
+print("Greater than or equal to in Strings:", str1 >= str2)
+
+#************************Less than or equal to*********************
+
+#Less than or equal to in numbers
+print("Less than or equal to in numbers:", num1 <= num2)
+
+#Less than or equal to in Strings
+print("Less than or equal to in strings:", str1 <= str2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
