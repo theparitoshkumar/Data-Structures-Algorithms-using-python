eddb6a4aaef45d38e0190bc4a361092a66389c9b
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-11 16:33:31 +0000

    membership operator in python

---
+++ 1. Python/1. Getting Started with Python/22.membership_operator.py
@@ -0,0 +1,13 @@
+#***************************Membership Operators************************
+
+#**************************in************************
+#Returns True if the variable/value is found in the specified sequence and False otherwise
+arr = [1,2,3,4,5]
+print("2 in arr:",2 in arr)
+print("'1' in arr:",'1' in arr)
+
+#***********************not in**************************
+#Returns True if the variable/value is not found in the specified sequence and False otherwise
+
+print("10 not in arr:",10 not in arr)
+print("1 not in arr:",1 not in arr)
