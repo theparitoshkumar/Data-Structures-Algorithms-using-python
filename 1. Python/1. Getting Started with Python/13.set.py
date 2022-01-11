f84fa0a6eb7631ccfdbc76b1d4463cc344838333
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-11 15:02:09 +0000

    set in python

---
+++ 1. Python/1. Getting Started with Python/13.set.py
@@ -0,0 +1,10 @@
+"""
+Set is an unordered collection of items separated by commas and the items are enclosed in curly brackets { }.
+A set is similar to list, except that it cannot have duplicate entries.
+Once created, elements of a set cannot be changed.
+"""
+
+# create a set
+set1 = {1,1,0,-6,4.5,"Hello"}
+# print set1
+print(set1) # {0, 1, 'Hello', 4.5, -6}
