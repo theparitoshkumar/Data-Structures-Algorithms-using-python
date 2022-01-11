15e4ac998851b9bf51ff4cc33868291b71055ac5
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-11 14:59:15 +0000

    tuple in python

---
+++ 1. Python/1. Getting Started with Python/12.tuple.py
@@ -0,0 +1,7 @@
+"""Tuple is a sequence of items separated by commas and items are enclosed in parenthesis ( ). 
+This is unlike list, where values are enclosed in brackets [ ]. Once created, we cannot change the tuple."""
+
+# Create a tuple
+tup = (5,4.3,-6,"New Delhi", "15C")
+# print tuples
+print(tup)
