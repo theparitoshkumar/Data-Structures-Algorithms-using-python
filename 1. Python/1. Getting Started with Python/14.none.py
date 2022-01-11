1874de21ec1abe1f948c61399d4411a11d1c17f3
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-11 15:05:28 +0000

    none in pyhton

---
+++ 1. Python/1. Getting Started with Python/14.none.py
@@ -0,0 +1,9 @@
+"""
+None is a special data type with a single value.
+It is used to signify the absence of value in a situation.
+None supports no special operations, and it is neither False nor 0 (zero).
+"""
+
+var = None
+print(type(var))
+print(var)
