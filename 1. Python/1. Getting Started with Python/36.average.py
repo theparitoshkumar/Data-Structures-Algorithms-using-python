17202d068ff72622014decd47d0cf5e331580a68
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-12 18:28:35 +0000

    Calculate average of three numbers

---
+++ 1. Python/1. Getting Started with Python/36.average.py
@@ -0,0 +1,8 @@
+"""Write a program to find average of three numbers."""
+
+num1 = int(input("Enter the number: "))
+num2 = int(input("Enter the number: "))
+num3 = int(input("Enter the number: "))
+
+avg = (num1+num2+num3)/3 
+print("Average:",avg)
