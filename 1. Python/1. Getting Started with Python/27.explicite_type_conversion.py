ff6a35d8780575275d2aeb846ae2dfba763fc3cd
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-12 16:04:01 +0000

    explicit conversion

--- 1. Python/1. Getting Started with Python/27.explicite_type_conversion.py
+++ 1. Python/1. Getting Started with Python/27.explicite_type_conversion.py
@@ -7,3 +7,20 @@ print(type(num3))
 num4 = float(num1 + num2)
 print(num4)
 print(type(num4))
+
+#Explicit type conversion from float to int
+num1 = 10.2
+num2 = 20.6
+num3 = (num1 + num2)
+print(num3)
+print(type(num3))
+num4 = int(num1 + num2)
+print(num4)
+print(type(num4))
+
+#Type Conversion between Numbers and Strings
+priceIcecream = 25
+priceBrownie = 45
+totalPrice = priceIcecream + priceBrownie
+print("The total is Rs." + str(totalPrice) )
+
