100f8563add293084c56c7fab15016c24aa1823f
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-12 18:18:25 +0000

    Swap two numbers

---
+++ 1. Python/1. Getting Started with Python/33.swap_numbers.py
@@ -0,0 +1,11 @@
+"""Write a program to swap two numbers using a third variable."""
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+num1 = temp
+num1 = num2
+num2 = temp
+
+print("First Number:",num1)
+print("Second Number:",num2)
