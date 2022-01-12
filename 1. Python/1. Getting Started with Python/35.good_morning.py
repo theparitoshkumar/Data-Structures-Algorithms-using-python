25892063e564b7131d9d740b6147d9c502859a47
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-12 18:25:53 +0000

    print GOOD MORNING without using loops

---
+++ 1. Python/1. Getting Started with Python/35.good_morning.py
@@ -0,0 +1,6 @@
+"""Write a program to repeat the string ‘‘GOOD MORNING” n times. Here ‘n’ is an integer entered by the user."""
+
+num = int(input("Enter the number: "))
+msg = "GOOD MORNING " * num
+
+print(msg)
