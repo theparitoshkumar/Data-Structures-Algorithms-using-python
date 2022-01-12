7c3f7fb9acff7ba06ceed2ff93b74aff326024ad
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-12 16:05:34 +0000

    implicit type conversion program

---
+++ 1. Python/1. Getting Started with Python/28.implicit_type_conversion.py
@@ -0,0 +1,6 @@
+#Implicit type conversion from int to float
+num1 = 10 #num1 is an integer
+num2 = 20.0 #num2 is a float
+sum1 = num1 + num2 #sum1 is sum of a float and an integer
+print(sum1)
+print(type(sum1))
