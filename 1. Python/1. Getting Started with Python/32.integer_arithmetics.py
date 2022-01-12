af3d0794f72574662e2f014df2117b52e98d7fd5
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-12 18:15:05 +0000

    Perform all arithmetic operations on two integer numbers

---
+++ 1. Python/1. Getting Started with Python/32.integer_arithmetics.py
@@ -0,0 +1,11 @@
+"""Write a program to enter two integers and perform all arithmetic operations on them."""
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Addition:",num1+num2)
+print("Subtraction:",num1-num2)
+print("Multiplication:",num1*num2)
+print("Division:",num1/num2)
+print("Modulus:",num1%num2)
+print("Exponetiation:",num1**num2)
+print("Floor Division:",num1//num2)
