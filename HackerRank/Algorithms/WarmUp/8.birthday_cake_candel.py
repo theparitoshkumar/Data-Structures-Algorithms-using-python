812c4afd3ea442a77738f9b2965607dbf793a1a9
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-13 16:21:06 +0000

    find repeating of maximum height candles

---
+++ HackerRank/Algorithms/WarmUp/8.birthday_cake_candel.py
@@ -0,0 +1,42 @@
+"""
+https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    max_hieght =max(candles)
+    count = 0
+    
+    for candel in candles:
+        if candel == max_hieght:
+            count += 1
+    
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
