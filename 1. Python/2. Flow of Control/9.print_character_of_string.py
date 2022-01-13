75342389e17905b8cd57f7dda31a7d7ea802ab7d
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-13 14:37:01 +0000

    Print all the character in 'PYTHON' using for loop

---
+++ 1. Python/2. Flow of Control/9.print_character_of_string.py
@@ -0,0 +1,10 @@
+"""
+Syntax of the For Loop
+for <control-variable> in <sequence/ items in range>:
+  <statements inside body of the loop>
+"""
+
+#Print the characters in word PYTHON using for loop
+
+for char in "PYTHON":
+    print(char)
