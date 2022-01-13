be1828e20927d702c9e620d1a068302c6c6c7bb3
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-13 14:43:13 +0000

    Print even numbers in the given sequence

---
+++ 1. Python/2. Flow of Control/11.even_number_from_list.py
@@ -0,0 +1,8 @@
+#Program to print even numbers in a given sequence using for loop.
+#Print even numbers in the given sequence
+
+list1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+for num in list1:
+    if num % 2 == 0:
+        print(num,"is even number.")
