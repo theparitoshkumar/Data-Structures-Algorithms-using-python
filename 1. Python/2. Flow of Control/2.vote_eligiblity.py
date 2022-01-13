5e16797b259dfd8e3e4a12ea8e038347a912e727
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-13 13:35:57 +0000

    Program to now the user id 18+

---
+++ 1. Python/2. Flow of Control/2.vote_eligiblity.py
@@ -0,0 +1,10 @@
+"""
+The syntax of if statement is:
+if condition:
+  statement(s)
+"""
+#Program to find the user is Eligible for vote or not
+age = int(input("Enter your age: "))
+
+if age > 18:
+    print("Eligible for Vote.")
