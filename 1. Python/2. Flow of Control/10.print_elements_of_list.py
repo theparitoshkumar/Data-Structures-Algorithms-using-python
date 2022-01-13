f3a8ba52d898e1d5e6babaec36fabcc7f605d470
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-13 14:39:33 +0000

    print every element of a list using for loop

---
+++ 1. Python/2. Flow of Control/10.print_elements_of_list.py
@@ -0,0 +1,7 @@
+# Program to print the numbers in a given sequence using for loop.
+#Print the given sequence of numbers using for loop
+
+list1 = [10,20,30,40,50]
+
+for num in list1:
+    print(num)
