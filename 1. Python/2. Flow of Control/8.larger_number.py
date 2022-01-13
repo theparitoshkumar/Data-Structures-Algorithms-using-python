3eb06b8e62ab1de2d53f45cebd0fadce57e5a49d
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-13 14:24:58 +0000

    find the larger number among two numbers and also understand the indentation

---
+++ 1. Python/2. Flow of Control/8.larger_number.py
@@ -0,0 +1,9 @@
+#Program to find larger of the two numbers
+num1 = 5
+num2 = 6
+if num1 > num2: #Block1
+        print("first number is larger")
+        print("Bye")
+else: #Block2
+        print("second number is larger")
+        print("Bye Bye")
