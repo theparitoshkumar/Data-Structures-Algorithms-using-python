e9cd5473b1b5143579d35f9eb997e45618c510ef
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2022-01-14 13:06:37 +0000

    print 5 natural number using while loop

---
+++ 1. Python/2. Flow of Control/15.print_natural_number.py
@@ -0,0 +1,15 @@
+"""
+Syntax of while Loop
+
+while test_condition:
+    body of while
+"""
+
+#Print first 5 natural numbers using while loop
+
+count = 1 
+while count <= 5:
+    print(count)
+    count += 1 
+    
+    
